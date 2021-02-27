24f897bfea8a7803badd685c4a2f0e85f7a79fad
Bruce D'Arcus <bdarcus@gmail.com>
2021-02-27 14:47:30 -0500

    Add package file template

---
+++ bibtex-actions.el
@@ -0,0 +1,25 @@
+;;; bibtex-actions.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2021 Bruce D'Arcus, Maxime Treca
+;;
+;; Author: Bruce D'Arcus <https://github.com/bdarcus>
+;; Author: Maxime Treca <https://github.com/mtreca>
+;; Created: February 27, 2021
+;; Modified: February 27, 2021
+;; Version: 0.0.1
+;; Keywords: bibtex, citations, bibliographies, academic
+;; Homepage: https://github.com/bdarcus/bibtex-actions
+;; Package-Requires: ((emacs "27.1") (bibtex-completion "1.0"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'bibtex-actions)
+;;; bibtex-actions.el ends here
