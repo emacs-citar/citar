f62e7aae89984e14c09277c4eedafd7463cdef0f
Bruce D'Arcus <bdarcus@gmail.com>
2021-03-02 14:07:24 -0500

    Add core completion functions

--- bibtex-actions.el
+++ bibtex-actions.el
@@ -19,7 +19,42 @@
 ;;
 ;;; Code:
 
+(require 'bibtex-completion)
 
+;; one can reset the backend function using fset; maybe there's a
+;; more elegant way to do this?
+(fset 'bibtex-actions-read-backend `bibtex-actions--completing-read)
+
+(defun bibtex-actions--read ()
+  "Select BibTeX entries in completion system."
+  ;; define a completion function that defaults to completing-read, but can be overridden
+  (list (bibtex-actions-read-backend)))
+
+(defun bibtex-actions--completing-read ()
+  "Read bibtex-completion entries for completion using completing-read."
+  (bibtex-completion-init)
+  (when-let ((candidates (bibtex-actions--get-candidates))
+             (chosen
+              (completing-read
+               "BibTeX entries: "
+               (lambda (string predicate action)
+                 (if (eq action 'metadata)
+                     '(metadata
+                       ;; (annotation-function . bibtex-completion--annotation)
+                       (category . bibtex))
+                   (complete-with-action action candidates string predicate))))))
+    (cdr (assoc chosen candidates))))
+
+(defun bibtex-actions--get-candidates ()
+  "Return all keys from bibtex-completion-candidates."
+  (cl-loop
+   for candidate in (bibtex-completion-candidates)
+   collect
+   (cons
+    ;; Here use one string for display, and the other for search.
+    (propertize
+     (car candidate) 'display (bibtex-completion-format-entry candidate (1- (frame-width))))
+    (cdr (assoc "=key=" candidate)))))
 
 (provide 'bibtex-actions)
 ;;; bibtex-actions.el ends here
