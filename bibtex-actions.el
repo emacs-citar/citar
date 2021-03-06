d09130c98911e30a096fb355cee61ef260acf2a3
Bruce D'Arcus <bdarcus@gmail.com>
2021-03-06 18:48:56 -0500

    Add license

--- bibtex-actions.el
+++ bibtex-actions.el
@@ -4,13 +4,26 @@
 ;;
 ;; Author: Bruce D'Arcus <https://github.com/bdarcus>, Maxime Treca <https://github.com/mtreca>
 ;; Created: February 27, 2021
-;; Modified: March 3, 2021
+;; License: GPL-3.0-or-later
 ;; Version: 0.0
 ;; Keywords: bib, files
 ;; Homepage: https://github.com/bdarcus/bibtex-actions
 ;; Package-Requires: ((emacs "26.3") (bibtex-completion "1.0"))
 ;;
 ;; This file is not part of GNU Emacs.
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;
 ;;; Commentary:
 ;;
