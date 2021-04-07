98931a876d56430b472f63b3f683a7d140b94ca6
Bruce D'Arcus <bdarcus@gmail.com>
2021-04-06 22:11:39 -0400

    Update bibtex-actions.el

--- bibtex-actions.el
+++ bibtex-actions.el
@@ -1,4 +1,4 @@
-;;; bibtex-actions.el --- A biblography manager based on completing-read -*- lexical-binding: t; -*-
+;;; bibtex-actions.el --- Biblographic commands based on completing-read -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (C) 2021 Bruce D'Arcus
 ;;
