1bdca9e846580752b25669d53bc536a00f40960c
Bruce D'Arcus <bdarcus@gmail.com>
2021-04-19 14:06:23 -0400

    add shebang to run.sh

--- test/run.sh
+++ test/run.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # adapted from https://github.com/raxod502/selectrum/tree/master/test
 /usr/bin/env \
     emacs -q --no-site-file --no-site-lisp --no-splash -l install.el -l bibtex-actions.el
