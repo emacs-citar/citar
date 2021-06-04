d4da79198537b2e95c032ec7b7a3d670f8d36772
Liu Hui <ilupin@users.noreply.github.com>
2021-06-04 17:37:43 +0800

    Add docstring

--- bibtex-actions.el
+++ bibtex-actions.el
@@ -134,7 +134,12 @@ you can use the following initial inputs: \"has:pdf\",
   :type '(alist :key-type symbol
                 :value-type (choice string
                                     (const :tag "No initial input" nil))))
-  
+
+(defcustom bibtex-actions-default-action 'bibtex-actions-open
+  "The default action for the `bibtex-actions-at-point' command."
+  :group 'bibtex-actions
+  :type 'function)
+    
 ;;; History, including future history list.
 
 (defvar bibtex-actions-history nil
@@ -493,10 +498,12 @@ With prefix, rebuild the cache before offering candidates."
   (interactive (list (bibtex-actions-read :rebuild-cache current-prefix-arg)))
   (bibtex-completion-add-pdf-to-library keys))
 
-(defvar bibtex-actions-default-action 'bibtex-actions-open)
-
 ;;;###autoload
 (defun bibtex-actions-at-point (keys)
+  "Select an action for the bibtex key at point.
+The bibtex key is found by `bibtex-completion-key-at-point', and
+the default action is set by `bibtex-actions-default-action'.
+With prefix, rebuild the cache before offering candidates."  
   (interactive (list (bibtex-actions-read :initial 'point
 					  :rebuild-cache current-prefix-arg)))
   (funcall bibtex-actions-default-action keys))
