53850dfdc5319eee5fba516340639ef904927931
Bruce D'Arcus <bdarcus@gmail.com>
2021-07-11 05:37:42 -0400

    comment out insert processor

--- bibtex-actions.el
+++ bibtex-actions.el
@@ -479,9 +479,9 @@ TEMPLATE."
 
 (when (require 'oc nil t)
   (org-cite-register-processor 'bibtex-actions
-    :insert (org-cite-make-insert-processor
-             (bibtex-actions-org-cite-insert t)
-             #'org-cite-basic--complete-style)
+    ;:insert (org-cite-make-insert-processor
+    ;         #'bibtex-actions-org-cite-insert
+    ;         #'org-cite-basic--complete-style)
     :follow (lambda (_datum _arg) (call-interactively 'bibtex-actions-at-point))))
 
 ;;; Embark
