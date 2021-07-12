b977f5449f3c0e8f2fb0c8062a8859e35a0ae60a
Bruce D'Arcus <bdarcus@gmail.com>
2021-07-12 07:19:16 -0400

    Simplify and generalize at-point

--- bibtex-actions.el
+++ bibtex-actions.el
@@ -161,12 +161,10 @@ means no action."
   :type '(choice (const :tag "Prompt" 'prompt)
                  (const :tag "Ignore" nil)))
 
-(defcustom bibtex-actions-embark-dwim t
-  "Whether to run the default action on citation keys at point.
-If non-nil, call the default embark action through `embark-dwim'.
-If nil, prompt the user for an action through `embark-act'."
+(defcustom bibtex-actions-at-point-function 'embark-dwim
+  "The function to run for 'bibtex-actions-at-point'."
   :group 'bibtex-actions
-  :type 'boolean)
+  :type 'function)
 
 ;;; History, including future history list.
 
