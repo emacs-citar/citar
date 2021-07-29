840d230617334a152e5acf7435bc896828b1ae88
Bruce D'Arcus <bdarcus@gmail.com>
2021-07-29 07:05:55 -0400

    Fix bibtex-actions-dwim
    Specify the cdr of the key cons.

--- bibtex-actions.el
+++ bibtex-actions.el
@@ -543,7 +543,7 @@ With prefix, rebuild the cache before offering candidates."
 (defun bibtex-actions-dwim ()
   "Run the default action on citation keys found at point."
   (interactive)
-  (if-let ((keys (bibtex-actions-citation-key-at-point)))
+  (if-let ((keys (cdr (bibtex-actions-citation-key-at-point))))
       (funcall bibtex-actions-default-action keys)))
 
 (provide 'bibtex-actions)
