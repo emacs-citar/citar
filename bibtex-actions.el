026591a2197de683c04669c48663ea6bc91f1bee
Bruce D'Arcus <bdarcus@gmail.com>
2021-07-30 01:25:11 -0400

    Update bibtex-actions.el
    Co-authored-by: Liu Hui <ilupin@users.noreply.github.com>

--- bibtex-actions.el
+++ bibtex-actions.el
@@ -537,14 +537,17 @@ With prefix, rebuild the cache before offering candidates."
 (defun bibtex-actions-run-default-action (keys)
   "Run the default action `bibtex-actions-default-action' on KEYS."
   (funcall bibtex-actions-default-action
-           (if (stringp keys) (split-string keys " & ") keys)))
+           (if (stringp keys) (split-string keys " & ")
+             (if (assoc 'citation-key keys)
+                 (list (cdr (assoc 'citation-key keys)))
+               keys))))
 
 ;;;###autoload
 (defun bibtex-actions-dwim ()
   "Run the default action on citation keys found at point."
   (interactive)
-  (if-let ((keys (cdr (bibtex-actions-citation-key-at-point))))
-      (funcall bibtex-actions-default-action keys)))
+  (if-let ((keys (bibtex-actions-citation-key-at-point)))
+      (bibtex-actions-run-default-action keys)))
 
 (provide 'bibtex-actions)
 ;;; bibtex-actions.el ends here
