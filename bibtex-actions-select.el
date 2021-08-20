e702edeb08d3474d7b2e26697bf374b7ff611752
Bruce D'Arcus <bdarcus@gmail.com>
2021-08-20 09:20:17 -0400

    Add bibtex-actions-select
    Not sure if this is a good idea, but it would include all the completion
    and related functions.

---
+++ bibtex-actions-select.el
@@ -0,0 +1,24 @@
+;;; bibtex-actions-select.el --- completion functions for bibtex-actions -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2021 Bruce D'Arcus
+;;
+;; Author: Bruce D'Arcus <https://github.com/bdarcus>
+;; Maintainer: Bruce D'Arcus <bdarcus@gmail.com>
+;; Created: August 20, 2021
+;; Modified: August 20, 2021
+;; Version: 0.4
+;; Homepage: https://github.com/bdarcus/bibtex-actions
+;; Package-Requires: ((emacs "26.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Completion functions to select citation keys and related files.
+;;
+;;; Code:
+
+
+
+(provide 'bibtex-actions-select)
+;;; bibtex-actions-select.el ends here
