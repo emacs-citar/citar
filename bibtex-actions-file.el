f04a4f7c1e144bc3064945bcb97d14f96efcd032
thisirs <thisirs@gmail.com>
2021-09-14 16:54:46 +0200

    Use list of functions in `bibtex-actions-file-parser-function(s)'

--- bibtex-actions-file.el
+++ bibtex-actions-file.el
@@ -53,11 +53,11 @@ use 'orb-edit-note' for this value."
   :group 'bibtex-actions
   :type '(function))
 
-(defcustom bibtex-actions-file-parser-function
-  'bibtex-actions-file-parser-default
-  "Function to parse file field."
+(defcustom bibtex-actions-file-parser-functions
+  '(bibtex-actions-file-parser-default)
+  "List of functions to parse file field."
   :group 'bibtex-actions
-  :type '(function))
+  :type '(repeat function))
 
 (defcustom bibtex-actions-file-extensions '("pdf" "org" "md")
   "A list of file extensions to recognize for related files."
@@ -107,7 +107,11 @@ use 'orb-edit-note' for this value."
            (file-field (bibtex-actions-get-value
                         bibtex-actions-file-variable entry))
            (results-file
-            (when file-field (funcall bibtex-actions-file-parser-function dirs file-field))))
+            (when file-field
+              (seq-mapcat
+               (lambda (func)
+                 (funcall func dirs file-field))
+               bibtex-actions-file-parser-functions))))
       (append results-key results-file))))
 
 (defun bibtex-actions-file--files-for-entry (entry dirs extensions)
