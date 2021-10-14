a89f11c47c05dd8ee05c93450330cf3a97002f21
Bruce D'Arcus <bdarcus@gmail.com>
2021-10-14 08:08:12 -0400

    cl-case -> pcase

--- bibtex-actions-filenotify.el
+++ bibtex-actions-filenotify.el
@@ -78,15 +78,15 @@ If it is other than 'global or 'local invalidate both"
   "The callback FUNC by SCOPE used to update cache for default options.
 
 CHANGE refers to the notify argument."
-  ;; FIX pcase instead
-  (cl-case (cadr change)
-    ((nil changed) (funcall func scope))
-    ((created deleted renamed) (if (member (nth 2 change)
-                                           (seq-concatenate 'list
-                                                            bibtex-actions-bibliography
-                                                            (bibtex-actions--local-files-to-cache)))
-                                   (bibtex-actions-filenotify-refresh scope)
-                                 (funcall func scope)))))
+  (pcase (cadr change)
+    ('(nil changed) (funcall func scope))
+    ('(created deleted renamed)
+     (if (member (nth 2 change)
+                 (seq-concatenate 'list
+                                  bibtex-actions-bibliography
+                                  (bibtex-actions--local-files-to-cache)))
+         (bibtex-actions-filenotify-refresh scope)
+       (funcall func scope)))))
 
 (defun bibtex-actions-filenotify--callback (scope &optional change)
   "A by SCOPE callback according to `bibtex-actions-filenotify-callback'.
@@ -94,13 +94,12 @@ CHANGE refers to the notify argument."
 This callback can be passed to the `file-notify-add-watch'.
 
 CHANGE refers to the filenotify argument."
-  ;; FIX use pcase instead
-  (cl-case bibtex-actions-filenotify-callback
-    (invalidate-cache (bibtex-actions-filenotify--make-default-callback
-                       #'bibtex-actions-filenotify--invalidate-cache scope change))
-    (refresh-cache (bibtex-actions-filenotify--make-default-callback
-                    (lambda (x) (bibtex-actions-refresh nil x)) scope change))
-    (t (funcall bibtex-actions-filenotify-callback scope change))))
+  (pcase bibtex-actions-filenotify-callback
+    ('invalidate-cache (bibtex-actions-filenotify--make-default-callback
+                        #'bibtex-actions-filenotify--invalidate-cache scope change))
+    ('refresh-cache (bibtex-actions-filenotify--make-default-callback
+                     (lambda (x) (bibtex-actions-refresh nil x)) scope change))
+    (_ (funcall bibtex-actions-filenotify-callback scope change))))
 
 (defun bibtex-actions-filenotify--add-local-watches ()
   "Add watches for the files that contribute to the local cache."
@@ -134,12 +133,11 @@ function can run several times without adding duplicate watches."
   "Get the list of files to watch from `bibtex-actions-filenotify-files'."
   (seq-mapcat (lambda (x)
                 (bibtex-actions-file--normalize-paths
-                 ;; FIX use pcase instead
-                 (cl-case x
-                   (bibliography bibtex-actions-bibliography)
-                   (library bibtex-actions-library-paths)
-                   (notes  bibtex-actions-notes-paths)
-                   (t x))))
+                 (pcase x
+                   ('bibliography bibtex-actions-bibliography)
+                   ('library bibtex-actions-library-paths)
+                   ('notes  bibtex-actions-notes-paths)
+                   (_ x))))
               bibtex-actions-filenotify-files))
 
 (defun bibtex-actions-filenotify-global-watches ()
