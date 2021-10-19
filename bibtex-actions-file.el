59ec3c6ea51f110d7e915fdee7fcd42fc068fbbd
Bruce D'Arcus <bdarcus@gmail.com>
2021-10-19 15:14:08 -0400

    update docs, change defcustom

--- bibtex-actions-file.el
+++ bibtex-actions-file.el
@@ -58,12 +58,11 @@ If you use 'org-roam' and 'org-roam-bibtex', you can use
   :group 'bibtex-actions
   :type '(function))
 
-(defcustom bibtex-actions-file-note-org-type
-  'default
+(defcustom bibtex-actions-file-note-org-include nil
   "The org note type."
   :group 'bibtex-actions
-  :type '(choice (const :tag "Default" 'default)
-                 (const :tag "Org-Roam" 'org-roam)))
+  :type '(repeat (const :tag "Org ID" 'org-id)
+                 (const :tag "Org-Roam :ROAM_REF:" 'org-roam-ref)))
 
 (defcustom bibtex-actions-file-parser-functions
   '(bibtex-actions-file-parser-default)
@@ -194,10 +193,16 @@ Example: ':/path/to/test.pdf:PDF'."
               (bibtex-actions--format-entry-no-widths
                entry
                template)))
-           (org-roam-key (when (equal 'org-roam bibtex-actions-file-note-org-type)
+           (org-id (when (member 'org-id bibtex-actions-file-note-org-include)
+                     (concat "\n:ID:   " uuid)))
+           (org-roam-key (when (member 'org-roam-ref bibtex-actions-file-note-org-include)
                            (concat "\n:ROAM_REFS: cite:" key)))
+           (prop-drawer (or org-id org-roam-key))
            (content
-            (concat ":PROPERTIES:" org-roam-key "\n:ID:  " uuid "\n:END:\n" note-meta "\n")))
+            (concat (when prop-drawer ":PROPERTIES:")
+                    org-roam-key org-id
+                    (when prop-drawer "\n:END:\n")
+                    note-meta "\n")))
       (funcall bibtex-actions-file-open-function file)
       ;; This just overrides other template insertion.
       (erase-buffer)
