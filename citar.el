d55cac7a2b8b848990ebd713de8667bf3fe93c6b
Bruce D'Arcus <bdarcus@gmail.com>
2022-05-26 08:15:12 -0400

    update to v0.9.5

--- citar.el
+++ citar.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Bruce D'Arcus <https://github.com/bdarcus>
 ;; Created: February 27, 2021
 ;; License: GPL-3.0-or-later
-;; Version: 0.9
+;; Version: 0.9.5
 ;; Homepage: https://github.com/bdarcus/citar
 ;; Package-Requires: ((emacs "27.1") (parsebib "3.0") (org "9.5") (citeproc "0.9"))
 
